b48b3a4c7042478f6ea3d2f43d4abbb8c405767e
Pranjul Singh <68863372+Ardemis69@users.noreply.github.com>
2022-09-16 13:40:15 -0400

    Week 2 Lesson comcplete

--- GameHub/Controllers/ShopController.cs
+++ GameHub/Controllers/ShopController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameHub.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameHub.Controllers
 {
@@ -7,6 +8,27 @@ namespace GameHub.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Category(string Name)
+        {
+            //,ensure some category name was passed
+            if (Name == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            //get the name parameter from the url and store in ViewData object so we can display object
+            ViewData["Category"] = Name;
+
+            //Use the product model to create the list of products in the memory to display
+            var products = new List<Product>();
+            //use a lopp to make some products
+            for(var i =1; i<6; i++)
+            {
+                products.Add(new Product { ProductId=i, Name = "Product"+ i.ToString(), Price= 10+i });
+            }
+            //loading the page and pass it the product list to display
+            return View(products);
         }
     }
 }
