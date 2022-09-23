7324a276ea03243fb161e6238e0cfce5c6ee27b4
Pranjul Singh <68863372+Ardemis69@users.noreply.github.com>
2022-09-23 11:58:03 -0400

    added the create view and controller

--- GameHub/Models/Product.cs
+++ GameHub/Models/Product.cs
@@ -1,9 +1,18 @@
-﻿namespace GameHub.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameHub.Models
 {
     public class Product
     {
         public int ProductId { get; set; }
+
+        // validation rules are written above the property
+        [MinLength (2, ErrorMessage ="Hey! This is way too Short")]
+        [MaxLength(100)]
+        [Required]
         public string? Name { get; set; }
+
+        [Range(0.01, 999999)]
         public decimal? Price { get; set; }
         
     }
